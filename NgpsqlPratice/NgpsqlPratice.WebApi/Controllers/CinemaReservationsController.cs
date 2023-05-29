1c6c30cc3a276e0118fd44c821b1d31d1e9a9675
Bruno Denisov <bruno.denisov9@gmail.com>
2023-05-29 09:59:08 +0200

    Created solution

---
+++ NgpsqlPratice/NgpsqlPratice.WebApi/Controllers/CinemaReservationsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NgpsqlPratice.WebApi.Controllers
+{
+    public class CinemaReservationsController : ApiController
+    {
+        // GET: api/CinemaReservations
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/CinemaReservations/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/CinemaReservations
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/CinemaReservations/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/CinemaReservations/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
