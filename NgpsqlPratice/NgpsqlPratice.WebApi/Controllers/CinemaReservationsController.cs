ea23d972310383aba174b8877a5eff59cd60ae4f
Bruno Denisov <bruno.denisov9@gmail.com>
2023-05-29 12:39:40 +0200

    Insert form body implemented

--- NgpsqlPratice/NgpsqlPratice.WebApi/Controllers/CinemaReservationsController.cs
+++ NgpsqlPratice/NgpsqlPratice.WebApi/Controllers/CinemaReservationsController.cs
@@ -9,6 +9,27 @@ using System.Web.Http;
 
 namespace NgpsqlPratice.WebApi.Controllers
 {
+    public class Costumer
+    {
+        public Guid Id { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Emial { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public Guid GuidGnerate()
+        {
+            Id = Guid.NewGuid();
+            return Id;
+        }
+    }
+
     public class CinemaReservationsController : ApiController
     {
         static string connString = "Server=localhost;Port=5432;User Id=postgres;Password=12345678;Database=CinemaReservations";
@@ -26,20 +47,25 @@ namespace NgpsqlPratice.WebApi.Controllers
         }
 
         // POST: api/CinemaReservations
-        public HttpResponseMessage Post()
+        public HttpResponseMessage Post([FromBody] Costumer costumer)
         {
-            Guid costumerGuid;
             Npgsql.NpgsqlConnection conn = new Npgsql.NpgsqlConnection(connString);
             using (conn)
             {
                 conn.Open();
                 NpgsqlCommand cmd = new NpgsqlCommand();
                 cmd.Connection = conn;
-                cmd.CommandText = "insert into costumer (id, first_name, last_name, gender, emial, phonenumber) values ('f504a1c2-2412-4ece-a81d-42039d228941'::UUID,'Jhon','Doe','M','jhon.doemail.com',553631421);";
+                cmd.CommandText = $"insert into costumer (Id, first_name, last_name, gender, emial, phonenumber) values (@Id,@first_name,@last_name,@gender,@emial,@phonenumber);";
+                cmd.Parameters.AddWithValue("Id", costumer.GuidGnerate());
+                cmd.Parameters.AddWithValue("first_name", costumer.First_Name);
+                cmd.Parameters.AddWithValue("last_name", costumer.Last_Name);
+                cmd.Parameters.AddWithValue("gender", costumer.Gender);
+                cmd.Parameters.AddWithValue("emial", costumer.Emial);
+                cmd.Parameters.AddWithValue("phonenumber", costumer.PhoneNumber);
                 int noRowsAffected = cmd.ExecuteNonQuery();
                 if(noRowsAffected > 0)
                 {
-                    return Request.CreateResponse(HttpStatusCode.OK);
+                    return Request.CreateResponse(HttpStatusCode.OK,"Row inserted");
                 }
             }
             return Request.CreateResponse(HttpStatusCode.BadRequest);
